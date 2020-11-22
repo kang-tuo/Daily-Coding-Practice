e450b54718591c84e7ec8c6bb2fcb5d9394b1f00
kang <v.kangt@gmail.com>
2020-11-22 13:12:01 +0100

    add day 1

---
+++ HackerRank/30 Days of Code/day 1 Data Types.py
@@ -0,0 +1,16 @@
+
+# Declare second integer, double, and String variables.
+sec_int = 3
+sec_double = 2.9
+sec_string ="hellllllllllllllo"
+# Read and save an integer, double, and String to your variables.
+sec_int=int(input())
+sec_double=float(input())
+sec_string=input()
+# Print the sum of both integer variables on a new line.
+print(i+sec_int)
+# Print the sum of the double variables on a new line.
+print(d+sec_double)
+# Concatenate and print the String variables on a new line
+print(s+sec_string)
+# The 's' variable above should be printed first.
